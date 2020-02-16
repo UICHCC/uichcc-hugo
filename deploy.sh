e677fd8499fc1ee27dc627773b92031e1f334316
ecwu <bitlabwzh@gmail.com>
2020-02-17 01:47:34 +0800

    CI: Build and push to uichcc.com

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+cd public
+
+if [ -n "$GITHUB_AUTH_SECRET" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+
+    git config credential.helper store
+    git config user.email "UICHCC-bot@users.noreply.github.com"
+    git config user.name "UICHCC-bot"
+fi
+
+
+
+git add .
+git commit -m "Rebuild site"
+git push --force origin HEAD:master
